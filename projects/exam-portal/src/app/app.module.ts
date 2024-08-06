1255c0bea0a7ccbbfcf30bb49f71c8b85a9014bc
sum7615 <sum793763@gmail.com>
2024-08-07 01:46:24 +0530

    content projection

--- projects/exam-portal/src/app/app.module.ts
+++ projects/exam-portal/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from '../components/home/home.component';
 import {HeaderComponent} from '../components/parts/header/header.component'
 import {FooterComponent} from '../components/parts/footer/footer.component'
+import { ContentProjectionComponent } from '../components/parts/content-projection/content-projection.component';
 
 @NgModule({
   declarations: [
     HomeComponent,HeaderComponent,FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule 
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [ContentProjectionComponent]
 })
 export class AppModule { }
